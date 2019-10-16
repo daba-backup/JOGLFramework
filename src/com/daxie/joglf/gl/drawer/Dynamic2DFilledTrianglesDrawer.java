0ac8081ef37965b5c2d3fc67ea7f0ad00262f72f
Dabasan <43123127+Dabasan@users.noreply.github.com>
2019-10-16 21:05:04 +0900

    Added a class to draw filled 2D triangles dynamically.

--- src/com/daxie/joglf/gl/drawer/Dynamic2DFilledTrianglesDrawer.java
+++ src/com/daxie/joglf/gl/drawer/Dynamic2DFilledTrianglesDrawer.java
@@ -16,14 +16,13 @@ import com.jogamp.common.nio.Buffers;
 import com.jogamp.opengl.GL4;
 
 /**
- * Draws filled quadrangles.
+ * Draws filled triangles.
  * @author Daba
  *
  */
-public class Dynamic2DFilledQuadranglesDrawer {
-	private Map<Integer, Vertex2D[]> quadrangles_map;
+public class Dynamic2DFilledTrianglesDrawer {
+	private Map<Integer, Vertex2D[]>triangles_map;
 	
-	private IntBuffer indices_vbo;
 	private IntBuffer pos_vbo;
 	private IntBuffer dif_vbo;
 	private IntBuffer vao;
@@ -31,15 +30,13 @@ public class Dynamic2DFilledQuadranglesDrawer {
 	private int window_width;
 	private int window_height;
 	
-	public Dynamic2DFilledQuadranglesDrawer() {
-		quadrangles_map=new TreeMap<>();
+	public Dynamic2DFilledTrianglesDrawer() {
+		triangles_map=new TreeMap<>();
 		
-		indices_vbo=Buffers.newDirectIntBuffer(1);
 		pos_vbo=Buffers.newDirectIntBuffer(1);
 		dif_vbo=Buffers.newDirectIntBuffer(1);
 		vao=Buffers.newDirectIntBuffer(1);
 		
-		GLWrapper.glGenBuffers(1, indices_vbo);
 		GLWrapper.glGenBuffers(1, pos_vbo);
 		GLWrapper.glGenBuffers(1, dif_vbo);
 		GLWrapper.glGenVertexArrays(1, vao);
@@ -49,19 +46,16 @@ public class Dynamic2DFilledQuadranglesDrawer {
 	}
 	
 	public void UpdateBuffers() {
-		int quadrangle_num=quadrangles_map.size();
-		int triangle_num=quadrangle_num*2;
-		int point_num=quadrangle_num*4;
+		int triangle_num=triangles_map.size();
+		int point_num=triangle_num*3;
 		
-		IntBuffer indices_buffer=Buffers.newDirectIntBuffer(triangle_num*3);
-		FloatBuffer pos_buffer=Buffers.newDirectFloatBuffer(point_num*2*2);
-		FloatBuffer dif_buffer=Buffers.newDirectFloatBuffer(point_num*4*2);
+		FloatBuffer pos_buffer=Buffers.newDirectFloatBuffer(point_num*2);
+		FloatBuffer dif_buffer=Buffers.newDirectFloatBuffer(point_num*4);
 		
-		int count=0;
-		for(Vertex2D[] quadrangle:quadrangles_map.values()) {
-			for(int i=0;i<4;i++) {
-				Point point=quadrangle[i].GetPoint();
-				ColorU8 dif=quadrangle[i].GetColor();
+		for(Vertex2D[] triangle:triangles_map.values()) {
+			for(int i=0;i<3;i++) {
+				Point point=triangle[i].GetPoint();
+				ColorU8 dif=triangle[i].GetColor();
 				
 				int x=point.x;
 				int y=point.y;
@@ -75,18 +69,8 @@ public class Dynamic2DFilledQuadranglesDrawer {
 				dif_buffer.put(dif.GetB());
 				dif_buffer.put(dif.GetA());
 			}
-			
-			indices_buffer.put(count);
-			indices_buffer.put(count+1);
-			indices_buffer.put(count+2);
-			
-			indices_buffer.put(count+2);
-			indices_buffer.put(count+3);
-			indices_buffer.put(count);
-			
-			count+=4;
 		}
-		((Buffer)indices_buffer).flip();
+		
 		((Buffer)pos_buffer).flip();
 		((Buffer)dif_buffer).flip();
 		
@@ -94,16 +78,12 @@ public class Dynamic2DFilledQuadranglesDrawer {
 		GLWrapper.glBufferData(GL4.GL_ARRAY_BUFFER, 
 				Buffers.SIZEOF_FLOAT*pos_buffer.capacity(), pos_buffer, GL4.GL_DYNAMIC_DRAW);
 		GLWrapper.glBindBuffer(GL4.GL_ARRAY_BUFFER, dif_vbo.get(0));
-		GLWrapper.glBufferData(GL4.GL_ARRAY_BUFFER, 
+		GLWrapper.glBufferData(GL4.GL_ARRAY_BUFFER,
 				Buffers.SIZEOF_FLOAT*dif_buffer.capacity(), dif_buffer, GL4.GL_DYNAMIC_DRAW);
 		
 		GLWrapper.glBindBuffer(GL4.GL_ARRAY_BUFFER, 0);
 		
 		GLWrapper.glBindVertexArray(vao.get(0));
-		
-		GLWrapper.glBindBuffer(GL4.GL_ELEMENT_ARRAY_BUFFER, indices_vbo.get(0));
-		GLWrapper.glBufferData(GL4.GL_ELEMENT_ARRAY_BUFFER, 
-				Buffers.SIZEOF_INT*indices_buffer.capacity(), indices_buffer, GL4.GL_DYNAMIC_DRAW);
 		
 		GLWrapper.glBindBuffer(GL4.GL_ARRAY_BUFFER, pos_vbo.get(0));
 		GLWrapper.glEnableVertexAttribArray(0);
@@ -117,35 +97,33 @@ public class Dynamic2DFilledQuadranglesDrawer {
 		GLWrapper.glBindVertexArray(0);
 	}
 	public void DeleteBuffers() {
-		GLWrapper.glDeleteBuffers(1, indices_vbo);
 		GLWrapper.glDeleteBuffers(1, pos_vbo);
 		GLWrapper.glDeleteBuffers(1, dif_vbo);
 		GLWrapper.glDeleteVertexArrays(1, vao);
 	}
 	
-	public void AddQuadrangle(int quadrangle_id,Vertex2D v1,Vertex2D v2,Vertex2D v3,Vertex2D v4) {
-		Vertex2D[] vertices=new Vertex2D[4];
+	public void AddTriangle(int triangle_id,Vertex2D v1,Vertex2D v2,Vertex2D v3) {
+		Vertex2D[] vertices=new Vertex2D[3];
 		vertices[0]=v1;
 		vertices[1]=v2;
 		vertices[2]=v3;
-		vertices[3]=v4;
 		
-		quadrangles_map.put(quadrangle_id, vertices);
+		triangles_map.put(triangle_id, vertices);
 	}
-	public int DeleteQuadrangle(int quadrangle_id) {
-		if(quadrangles_map.containsKey(quadrangle_id)==false) {
-			LogFile.WriteWarn("[Dynamic2DFilledQuadrangleDrawer-DeleteQuadrangle] ", true);
-			LogFile.WriteWarn("No such quadrangle. quadrangle_id:"+quadrangle_id, false);
+	public int DeleteTriangle(int triangle_id) {
+		if(triangles_map.containsKey(triangle_id)==false) {
+			LogFile.WriteWarn("[Dynamic2DFilledTrianglesDrawer-DeleteTriangle] ", true);
+			LogFile.WriteWarn("No such triangle. triangle_id:"+triangle_id, false);
 			
 			return -1;
 		}
 		
-		quadrangles_map.remove(quadrangle_id);
+		triangles_map.remove(triangle_id);
 		
 		return 0;
 	}
-	public void DeleteAllQuadrangles() {
-		quadrangles_map.clear();
+	public void DeleteAllTriangles() {
+		triangles_map.clear();
 	}
 	
 	public void SetWindowSize(int width,int height) {
@@ -158,12 +136,11 @@ public class Dynamic2DFilledQuadranglesDrawer {
 		
 		GLWrapper.glBindVertexArray(vao.get(0));
 		
-		int quadrangle_num=quadrangles_map.size();
-		int triangle_num=quadrangle_num*2;
-		int indices_size=triangle_num*3;
+		int triangle_num=triangles_map.size();
+		int point_num=triangle_num*3;
 		
 		GLWrapper.glEnable(GL4.GL_BLEND);
-		GLWrapper.glDrawElements(GL4.GL_TRIANGLES, indices_size, GL4.GL_UNSIGNED_INT, 0);
+		GLWrapper.glDrawArrays(GL4.GL_TRIANGLES, 0, point_num);
 		GLWrapper.glDisable(GL4.GL_BLEND);
 		
 		GLWrapper.glBindVertexArray(0);
