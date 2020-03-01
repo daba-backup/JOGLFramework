a853822d8ccadb4866d6ac24033983eae095d9c8
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-03-01 12:32:39 +0900

    Updated Screen.

--- joglframework-gl/src/com/daxie/joglf/gl/util/screen/Screen.java
+++ joglframework-gl/src/com/daxie/joglf/gl/util/screen/Screen.java
@@ -1,11 +1,19 @@
 package com.daxie.joglf.gl.util.screen;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+
+import javax.imageio.ImageIO;
 
 import com.daxie.joglf.gl.shader.ShaderProgram;
 import com.daxie.joglf.gl.transferrer.FullscreenQuadTransferrerWithUV;
 import com.daxie.joglf.gl.wrapper.GLWrapper;
 import com.daxie.log.LogWriter;
+import com.daxie.tool.ExceptionFunctions;
+import com.daxie.tool.FilenameFunctions;
 import com.jogamp.common.nio.Buffers;
 import com.jogamp.opengl.GL4;
 
@@ -113,5 +121,46 @@ public class Screen {
 	public void Draw(int x,int y,int width,int height) {
 		GLWrapper.glViewport(x, y, width, height);
 		this.Draw();
+	}
+	
+	public int TakeScreenshot(String filename) {
+		ByteBuffer data=Buffers.newDirectByteBuffer(screen_width*screen_height);
+		
+		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, texture_id);
+		GLWrapper.glGetTexImage(GL4.GL_TEXTURE_2D, 0, GL4.GL_RGBA, GL4.GL_UNSIGNED_BYTE, data);
+		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, 0);
+		
+		BufferedImage image=new BufferedImage(screen_width, screen_height, BufferedImage.TYPE_3BYTE_BGR);
+		
+		int pos=0;
+		for(int y=screen_height-1;y>=0;y--) {
+			for(int x=0;x<screen_width;x++) {
+				int r=Byte.toUnsignedInt(data.get(pos));
+				int g=Byte.toUnsignedInt(data.get(pos+1));
+				int b=Byte.toUnsignedInt(data.get(pos+2));
+				int a=Byte.toUnsignedInt(data.get(pos+3));
+				
+				int rgb=(a<<24)|(r<<16)|(g<<8)|b;
+				
+				image.setRGB(x, y, rgb);
+				
+				pos+=4;
+			}
+		}
+		
+		String extension=FilenameFunctions.GetFileExtension(filename);
+		try {
+			ImageIO.write(image, extension, new File(filename));
+		}
+		catch(IOException e) {
+			String str=ExceptionFunctions.GetPrintStackTraceString(e);
+			
+			LogWriter.WriteWarn("[Screen-TakeScreenshot] Below is the stack trace.", true);
+			LogWriter.WriteWarn(str, false);
+			
+			return -1;
+		}
+		
+		return 0;
 	}
 }
