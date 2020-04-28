2bfe32a35624429647005f0c922352afa8c2c7d3
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-04-28 14:11:21 +0900

    Added TestMain.

---
+++ joglframework-gl/src/test/java/com/github/dabasan/joglf/gl/TestMain.java
@@ -0,0 +1,14 @@
+package com.github.dabasan.joglf.gl;
+
+import com.github.dabasan.joglf.gl.window.JOGLFWindow;
+import com.github.dabasan.joglf.gl.window.JOGLFWindowInterface;
+
+public class TestMain {
+	public static void main(String[] args) {
+		new TestMain();
+	}
+	public TestMain() {
+		JOGLFWindowInterface window=new JOGLFWindow();
+		window.SetExitProcessWhenDestroyed();
+	}
+}
