dc81d3fdb311e4128411870a888c2b8e75a3a0c9
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-03 14:07:51 +0900

    Added test classes.

---
+++ joglframework-gl/src/test/java/com/github/dabasan/joglf/gl/Viewer.java
@@ -0,0 +1,54 @@
+package com.github.dabasan.joglf.gl;
+
+import com.github.dabasan.basis.vector.VectorFunctions;
+import com.github.dabasan.joglf.gl.draw.DrawFunctions3D;
+import com.github.dabasan.joglf.gl.input.keyboard.KeyboardEnum;
+import com.github.dabasan.joglf.gl.input.mouse.MouseEnum;
+import com.github.dabasan.joglf.gl.model.Model3DFunctions;
+import com.github.dabasan.joglf.gl.window.JOGLFWindow;
+
+public class Viewer extends JOGLFWindow {
+	private FreeCamera camera;
+
+	private int model_handle;
+
+	@Override
+	public void Init() {
+		camera = new FreeCamera();
+		camera.SetPosition(VectorFunctions.VGet(50.0f, 50.0f, 50.0f));
+
+		model_handle = Model3DFunctions
+				.LoadModel("./Data/Model/BD1/map0/temp.bd1");
+		Model3DFunctions.RescaleModel(model_handle,
+				VectorFunctions.VGet(0.1f, 0.1f, 0.1f));
+	}
+
+	@Override
+	public void Update() {
+		int w_pressing_count = this
+				.GetKeyboardPressingCount(KeyboardEnum.KEY_W);
+		int s_pressing_count = this
+				.GetKeyboardPressingCount(KeyboardEnum.KEY_S);
+		int d_pressing_count = this
+				.GetKeyboardPressingCount(KeyboardEnum.KEY_D);
+		int a_pressing_count = this
+				.GetKeyboardPressingCount(KeyboardEnum.KEY_A);
+		camera.Translate(w_pressing_count, s_pressing_count, d_pressing_count,
+				a_pressing_count);
+
+		if (this.GetMousePressingCount(MouseEnum.MOUSE_MIDDLE) > 0) {
+			int diff_x = this.GetCursorDiffX();
+			int diff_y = this.GetCursorDiffY();
+
+			camera.Rotate(diff_x, diff_y);
+		}
+
+		camera.Update();
+	}
+
+	@Override
+	public void Draw() {
+		Model3DFunctions.DrawModel(model_handle);
+		DrawFunctions3D.DrawAxes(100.0f);
+	}
+}
