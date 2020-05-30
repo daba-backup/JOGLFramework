deded5be13e594d4e77b4e4d9bacca323496eb34
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-30 22:36:11 +0900

    Updated texture parameters.

--- joglframework-gl/src/main/java/com/github/dabasan/joglf/gl/util/screen/ScreenWithDepth.java
+++ joglframework-gl/src/main/java/com/github/dabasan/joglf/gl/util/screen/ScreenWithDepth.java
@@ -42,8 +42,8 @@ public class ScreenWithDepth extends ScreenBase {
 		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, depth_texture_id);
 		GLWrapper.glTexImage2D(GL4.GL_TEXTURE_2D, 0, GL4.GL_DEPTH_COMPONENT, width, height, 0,
 				GL4.GL_DEPTH_COMPONENT, GL4.GL_FLOAT, null);
-		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MAG_FILTER, GL4.GL_LINEAR);
-		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MIN_FILTER, GL4.GL_LINEAR);
+		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MAG_FILTER, GL4.GL_NEAREST);
+		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_MIN_FILTER, GL4.GL_NEAREST);
 		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_WRAP_S, GL4.GL_CLAMP_TO_EDGE);
 		GLWrapper.glTexParameteri(GL4.GL_TEXTURE_2D, GL4.GL_TEXTURE_WRAP_T, GL4.GL_CLAMP_TO_EDGE);
 		GLWrapper.glBindTexture(GL4.GL_TEXTURE_2D, 0);
